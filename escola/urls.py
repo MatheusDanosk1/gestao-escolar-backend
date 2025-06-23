0c521e18c3586eaa53ada3e3cf59964879ed950f
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:37:25 -0300

    Adiciona rota para Professor na API

---
+++ escola/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AlunoViewSet, ProfessorViewSet,DisciplinaViewSet, NotaViewSet
+
+router = DefaultRouter()
+router.register(r'alunos', AlunoViewSet)
+router.register(r'disciplinas', DisciplinaViewSet)
+router.register(r'notas', NotaViewSet)
+router.register(r'professores', ProfessorViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
