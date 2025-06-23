1fdf60e8260f948f095435484b42e4b9535923c2
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:50:12 -0300

    Ajusta configuração da rota de Disciplina

--- escola/urls.py
+++ escola/urls.py
@@ -3,10 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import AlunoViewSet, ProfessorViewSet, NotaViewSet, DisciplinaViewSet
 
 router = DefaultRouter()
+router.register(r'disciplinas', DisciplinaViewSet)
 router.register(r'alunos', AlunoViewSet)
 router.register(r'notas', NotaViewSet)
 router.register(r'professores', ProfessorViewSet) 
-router.register(r'disciplinas', DisciplinaViewSet)
+
 
 
 urlpatterns = [
