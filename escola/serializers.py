89467e2a1f8956fc72b6b9e3c4c5dedecbd175f8
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:34:13 -0300

    Cria serializer para Professor

---
+++ escola/serializers.py
@@ -0,0 +1,9 @@
+
+from .models import Professor
+from rest_framework import serializers
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
