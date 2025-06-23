2af8a550cde78f1c49d48eb23024c27e6fbdf589
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:55:11 -0300

    Cria model Nota

--- escola/models.py
+++ escola/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-class Turma(models.Model):
-    nome = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nome
-
-class Aluno(models.Model):
-    nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=14, unique=True)
-    email = models.EmailField(unique=True)
-    matricula = models.CharField(max_length=20, unique=True)
-    curso = models.CharField(max_length=100)
-    turma = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name='alunos')
-
-    def __str__(self):
-        return self.nome
 
 class Professor(models.Model):
     nome = models.CharField(max_length=100)
