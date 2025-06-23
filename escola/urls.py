fba5af5d900cfb2327bef133c8cd5ab4abeadd02
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 19:12:21 -0300

    Ajusta configuração da URL de Turma

--- escola/urls.py
+++ escola/urls.py
@@ -7,8 +7,7 @@ router.register(r'disciplinas', DisciplinaViewSet)
 router.register(r'alunos', AlunoViewSet)
 router.register(r'professores', ProfessorViewSet) 
 router.register(r'notas', NotaViewSet)
-
-
+router.register(r'turmas', TurmaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
