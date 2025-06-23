32974888cebac2fce98cb0317ff239332a269a69
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 19:06:08 -0300

    Ajusta configuração da URL de Aluno

--- escola/urls.py
+++ escola/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AlunoViewSet, ProfessorViewSet, DisciplinaViewSet, NotaViewSet, AlunoViewSet
+from .views import AlunoViewSet, ProfessorViewSet, DisciplinaViewSet, NotaViewSet
 
 router = DefaultRouter()
 router.register(r'disciplinas', DisciplinaViewSet)
