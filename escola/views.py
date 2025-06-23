af7c4fb0f7e3a0a88e3abe81caf3f66ac5353bb1
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:57:18 -0300

    Ajusta queryset do ViewSet de Nota

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
+from .models import Aluno
+from .serializers import AlunoSerializer
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
 from .models import Professor
 from .serializers import ProfessorSerializer
