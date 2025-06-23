2970d6d39e904b55bd7731ceff5eb84046dd93e7
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-23 18:47:42 -0300

    Ajusta queryset da ViewSet de Disciplina

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import Aluno
 
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
 from .models import Professor
 from rest_framework import serializers
 
@@ -17,4 +22,9 @@ class DisciplinaSerializer(serializers.ModelSerializer):
         model = Disciplina
         fields = '__all__'
 
+from .models import Nota
 
+class NotaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nota
+        fields = '__all__'
